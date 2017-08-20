1245f433a9ffd35ce333316f4df486081facef5a
Aeger <extaeger@gmail.com>
2017-08-20 13:03:52 -0700

    Generalize the responses of the bot. Added a service to upload and backup responses. Added commands to add and upload responses to the bot.

--- service/wario_base.py
+++ service/wario_base.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import json
 import random
 import argparse
 
@@ -9,6 +10,9 @@ args = parser.parse_args()
 
 description = '''Wah?'''
 client = discord.Client()
+bot = commands.Bot(command_prefix='war?', description=description)
+
+RESPONSES = {}
 
 @client.event
 async def on_ready():
@@ -19,9 +23,25 @@ async def on_ready():
     
 @client.event
 async def on_message(message):
-    if 'justice' in message.content.lower():
-        await client.send_message(message.channel, 'http://www.nintendoworldreport.com/media/18935/4/1.jpg')
+    for keyword in RESPONSES:
+        if keyword.lower() in message.content.lower():
+            await client.send_message(message.channel, RESPONSES[keyword])
     if 'wario' in message.content.lower():
         await client.send_message(message.channel, 'Wah, wah, WA' + 'H'*int(random.uniform(1,1000)))
 
+@bot.command()
+async def update():
+    global RESPONSES
+    with open('wario.txt') as resource_file:
+      RESPONSES = json.load(resource_file)
+    await bot.say('Updated')
+
+@bot.command()
+async def add(keyword: str, response: str):
+    RESPONSES[keyword] = response
+    with open('data.txt', 'w') as resource_file:
+        json.dump(RESPONSES, resource_file)
+    await bot.say('Wah-dded response for ' + keyword)
+
 client.run(args.client_id)
+bot.run(args.client_id)
--- service/web/server.py
+++ service/web/server.py
@@ -1,10 +1,30 @@
-import http.server
-import socketserver
+from flask import Flask
+from flask import render_template
+from flask import redirect
+from flask import request
+from flask import send_file
+app = Flask(__name__)
 
-PORT = 8000
+@app.route('/')
+def hello_world():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == 'wario.txt':
+            file.save(file.filename)
+            return redirect(request.url)
+        else:
+            flash('Not wario.txt')
+            return redirect(request.url)
 
-Handler = http.server.SimpleHTTPRequestHandler
+    return render_template('upload_responses.html')
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+@app.route('/backup')
+def return_backup():
+    return send_file('wario.txt')
+
+if __name__ == '__main__':
+    app.run()
